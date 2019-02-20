51050e493489cc67fd034a3225130978f04bdfe8
servilad <servilad62@yandex.ru>
2019-02-20 22:40:12 +0300

    Готова программа части 3 урока 1

--- lesson1/lesson 1 - part 3/Program.cs
+++ lesson1/lesson 1 - part 3/Program.cs
@@ -10,6 +10,26 @@ namespace lesson_1___part_3
     {
         static void Main(string[] args)
         {
+            //программа расчета между точками
+            double x1 = 0;
+            double x2 = 0;
+            double y1 = 0;
+            double y2 = 0;
+            Console.WriteLine("Программа расчета математической формулы");
+            Console.WriteLine("Введите последовательно две координаты X и Y точки 1");
+            x1 = int.Parse(Console.ReadLine());
+            y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите последовательно две координаты X и Y точки 2");
+            x2 = int.Parse(Console.ReadLine());
+            y2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ответ  = {0:F2}", res(x1, y1, x2, y2));
+            Console.ReadLine();
+            
+        }
+        static double res(double x1,double y1,double x2, double y2)
+        {
+            double rezult = 0;
+            return rezult = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
         }
     }
 }
