71727e44022108b3086239b1fffaedeec0caa6cd
servilad <servilad62@yandex.ru>
2019-02-20 23:10:08 +0300

    Программа части 5 урока 1

--- lesson1/lesson1 - part 5/Program.cs
+++ lesson1/lesson1 - part 5/Program.cs
@@ -10,6 +10,33 @@ namespace lesson1___part_5
     {
         static void Main(string[] args)
         {
+            //Программа вывода на экран с разными координатами
+            string name = "";
+            string surname = "";
+            string town = "";
+            string rez = "";
+            int x;
+            int y;
+            Console.WriteLine("Программа форматированной вывода на экран данных пользователя, введите личные данные");
+            Console.WriteLine("Введите свое имя ");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите свою фамилию ");
+            surname = Console.ReadLine();
+            Console.WriteLine("Введите город в котором проживаете ");
+            town = Console.ReadLine();
+            Console.WriteLine("Введите положение по горизонтали ");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите положение по вертикали");
+            y = int.Parse(Console.ReadLine());
+            rez ="Ваше имя: <" + name + "> Ваша фамилия: <" + surname + "> Город в котором проживаете: <" + town + ">";
+            PrintXY(rez, x, y);
+            Console.ReadLine();
+        }
+
+        static void PrintXY(string msg, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(msg);
         }
     }
 }
