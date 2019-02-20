96f6a87fa633aac21056d078c168e6c3766190d5
servilad <servilad62@yandex.ru>
2019-02-20 22:04:54 +0300

    Программа №1 урока 1 готова

--- lesson1/lesson1/Program.cs
+++ lesson1/lesson1/Program.cs
@@ -10,6 +10,27 @@ namespace lesson1
     {
         static void Main(string[] args)
         {
+            //Программа АНКЕТА
+            string name = "";
+            string surname = "";
+            string age = "";
+            string high = "";
+            string weight = "";
+            Console.WriteLine("Программа АНКЕТА, введите личные данные");
+            Console.WriteLine("Введите свое имя ");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите свою фамилию ");
+            surname = Console.ReadLine();
+            Console.WriteLine("Введите свой возраст ");
+            age = Console.ReadLine();
+            Console.WriteLine("Введите свой рос ");
+            high = Console.ReadLine();
+            Console.WriteLine("Введите свой вес ");
+            weight = Console.ReadLine();
+
+            Console.WriteLine("Ваше имя: " + name + ",\nВаша фамилия: " + surname + ",\nВаш возраст: " + age + ",\nВаш рост: " + high + ",\nВаш вес: " + weight );
+            Console.ReadLine();
+
         }
     }
 }
