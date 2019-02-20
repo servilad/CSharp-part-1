de6a0bb67fd96f19fa99e53a0fa1944cda1ab3d7
servilad <servilad62@yandex.ru>
2019-02-20 22:52:36 +0300

    Решено задание части 4 урока 1

--- lesson1/lesson1 - part 4/Program.cs
+++ lesson1/lesson1 - part 4/Program.cs
@@ -10,6 +10,19 @@ namespace lesson1___part_4
     {
         static void Main(string[] args)
         {
+            //Программа обмена значениями в переменных
+            int a = 0;
+            int b = 0;
+            Console.WriteLine("програама обмена значений в переменных");
+            Console.WriteLine("Введите знчение целочисленной переменной a");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите знчение целочисленной переменной b");
+            b = int.Parse(Console.ReadLine());
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("значние a = " + a + "\nзначние b = " + b);
+            Console.ReadLine();
         }
     }
 }
